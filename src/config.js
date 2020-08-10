6517b1ca3ac80e5e2d585335cb88c9b57ec9be83
Kedar Nath <kedar170123023@iitg.ac.in>
2020-08-10 15:05:40 +0530

    update config error

--- src/config.js
+++ src/config.js
@@ -1,5 +1,9 @@
-export default {
-    MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://Navprayas:123@Kedar@cluster0-sb4wy.mongodb.net/Navprayas?retryWrites=true&w=majority",
+import dotenv from "dotenv";
+dotenv.config();
+
+
+export default {  
+    MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://Navprayas:123@Kedar@cluster0-sb4wy.mongodb.net/Navprayas?retryWrites=true&w=majority" ,
   };
 
   
