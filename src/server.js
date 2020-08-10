905610a2c9c750602e5b064899ddb313ad079b16
Kedar Nath <kedar170123023@iitg.ac.in>
2020-08-11 00:25:00 +0530

    Revert "modify config mongodb url"
    This reverts commit 291a5d7d4e91c5e29718658754890e3ce2c49445.

--- src/server.js
+++ src/server.js
@@ -12,8 +12,6 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-console.log(process.env)
-
 //---------------------------------------------------------------------
 // database 
 const mongodbUrl = config.MONGODB_URL;
@@ -41,5 +39,6 @@ app.get('/',(req, res)=>{
 
 
 // creating server and running
-app.listen(config.PORT, ()=> console.log(`server is running at http://localhost:${config.PORT}`))
+const port = process.env.PORT || 5000;
+app.listen(port, ()=> console.log(`server is running at http://localhost:${port}`))
 
