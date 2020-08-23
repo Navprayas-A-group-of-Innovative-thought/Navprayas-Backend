07bb203f0ebc86f5ea2f3826f938db916f94d7f5
Ashutosh Krishna <ashutoshbritish@gmail.com>
2020-08-23 18:00:01 +0530

    Add date format in message

--- src/validation/valid.js
+++ src/validation/valid.js
@@ -6,20 +6,21 @@ exports.validSignup = [
   check("firstName", "First Name is required.").notEmpty(),
   check("email").isEmail().withMessage("Must be a valid email address"),
   check("password", "Password is required")
-    .notEmpty()
     // .matches(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8, 20}$/)
+    .notEmpty()
     .withMessage(
       "Must contain atleast one lower case, one uppercase, one special character and one digit."
     ),
   check("confirmPassword", "Passwords do not match."),
-  check("dob").trim().isDate().withMessage("Must be a valid date."),
+  check("dob").trim().isDate().withMessage("Date should be in YYYY/MM/DD format"),
   check("gender").isIn(["Male", "Female"]),
 ];
 
 //Login Validation
 exports.validLogin = [
     check('email').isEmail().withMessage('Must be a valid email address'),
-    check('password', 'Password is required.').notEmpty()
+  check('password', 'Password is required.')
+    // .matches(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8, 20}$/)
 ];
 
 //Forgot Password Validation
@@ -32,8 +33,8 @@ exports.forgotPasswordValidator = [
 //Reset Password Validation
 exports.resetPasswordValidator = [
     check('newPassword', 'Password is required.')
+    // .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)
     .notEmpty()
-    // .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/, "i")
     .withMessage(
       "Must contain atleast one lower case, one uppercase, one special character and one digit."
     ),
