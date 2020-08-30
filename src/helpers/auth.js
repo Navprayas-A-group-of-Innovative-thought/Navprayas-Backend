e30a02700bc56671362008742a7b0b46cef5d479
Ashutosh Krishna <ashutoshbritish@gmail.com>
2020-08-31 01:01:36 +0530

    Add comments

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,14 +1,21 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
+// Middleware to check if the user is logged in or not
 const auth = (req, res, next) => {
   try {
+    // Extracting token from authorization in header and splitting it as Bearer and token id
     const token = req.headers.authorization.split(" ");
-      const user = jwt.verify(token[1], process.env.JWT_SECRET);
+    // Verifying the extracted token id with the secret key
+    const user = jwt.verify(token[1], process.env.JWT_SECRET);
     if (token[0] === "Bearer" && user) {
       next();
     }
   } catch (e) {
-    return res.status(401).json('You are not signed in. Please login or signup to continue');
+    return res
+      .status(401)
+      .json({
+        errorDetails: "You are not signed in. Please login   to continue",
+      });
   }
 };
 
