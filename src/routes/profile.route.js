f425216c14104f679ed5075066629587984abc36
Ashutosh Krishna <ashutoshbritish@gmail.com>
2020-08-31 12:11:22 +0530

    Add Password change route and validators

--- src/routes/profile.route.js
+++ src/routes/profile.route.js
@@ -1,15 +1,23 @@
 // Import libraries here
 const express = require("express");
 const router = express.Router();
-const authRequired = require('../helpers/auth')
+const authRequired = require("../helpers/auth");
 
 // Load controllers
-const { profileController, editController } = require("../controllers/profile.controller");
+const {
+  profileController,
+  editController,
+  passwordController,
+} = require("../controllers/profile.controller");
+
+// Validators here
+const {changePasswordValidator} = require("../validation/valid")
 
 // All profile related routers here
-router.use(authRequired)
+router.use(authRequired);
 router.get("/profile", profileController);
-router.get('/profile/edit', profileController)
-router.post('/profile/edit',editController)
+router.get("/profile/edit", profileController);
+router.post("/profile/edit", editController);
+router.post("/password/change",changePasswordValidator, passwordController);
 
 module.exports = router;
