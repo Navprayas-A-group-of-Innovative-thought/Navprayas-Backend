022a47c3385c2cf9aeda9e6e23e454c8e8fa8303
Ashutosh Krishna <ashutoshbritish@gmail.com>
2020-09-09 15:28:43 +0530

    Fix response

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -52,7 +52,7 @@ exports.profileController = (req, res) => {
         gender: gender,
         fatherName: fatherName,
         motherName: motherName,
-        contact:contact,
+        contact: contact,
         grade: grade,
         year: year,
         schoolOrUniv: schoolOrUniv,
@@ -117,10 +117,57 @@ exports.editController = (req, res) => {
             errorDetails: errorHandler(err),
           });
         } else {
+          var firstName = user.firstName;
+          var lastName = user.lastName;
+          var email = user.email;
+          var dob = user.dob;
+          var gender = user.gender;
+          var fatherName = user.profile.fatherName;
+          var motherName = user.profile.motherName;
+          var contact = user.profile.contact;
+          var grade = user.profile.education.grade;
+          var year = user.profile.education.year;
+          var schoolOrUniv = user.profile.education.schoolorUniv;
+          var instituteName = user.profile.education.instituteName;
+          var board = user.profile.education.board;
+          var houseNumber = user.profile.address.houseNumber;
+          var addressLine1 = user.profile.address.addressLine1;
+          var addressLine2 = user.profile.address.addressLine2;
+          var landmark = user.profile.address.landmark;
+          var district = user.profile.address.district;
+          var city = user.profile.address.city;
+          var pincode = user.profile.address.pincode;
+          var country = user.profile.address.country;
+          var facebookLink = user.profile.socialInfo.facebookLink;
+          var githubLink = user.profile.socialInfo.githubLink;
+          var linkedinLink = user.profile.socialInfo.linkedinLink;
           // else success
           return res.status(200).json({
             responseData: "Profile updated successfully.",
-            user,
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            dob: dob,
+            gender: gender,
+            fatherName: fatherName,
+            motherName: motherName,
+            contact: contact,
+            grade: grade,
+            year: year,
+            schoolOrUniv: schoolOrUniv,
+            instituteName: instituteName,
+            board: board,
+            houseNumber: houseNumber,
+            addressLine1: addressLine1,
+            addressLine2: addressLine2,
+            landmark: landmark,
+            district: district,
+            city: city,
+            pincode: pincode,
+            country: country,
+            facebookLink: facebookLink,
+            linkedinLink: linkedinLink,
+            githubLink: githubLink,
           });
         }
       });
@@ -132,45 +179,54 @@ exports.passwordController = (req, res) => {
   const token = req.headers.authorization.split(" "); // extracting token from header
   const { _id } = jwt.decode(token[1]); // decoding _id from token
   const { oldPassword, newchangePassword, confirmchangePassword } = req.body; //get oldPassword, newchangePassword, confirmchangePassword from the body
-  const errors = validationResult(req);         //validate the passwords
-  if (!errors.isEmpty()) {                      // if error
+  const errors = validationResult(req); //validate the passwords
+  if (!errors.isEmpty()) {
+    // if error
     const firstError = errors.array().map((error) => error.msg)[0];
     return res.status(422).json({
       errorDetails: firstError,
     });
-  } else {                                      // if no error
-    User.findOne({ _id }).exec((err, user) => {                 // search for user in database
-      if (err || !user) {                                       // if no user
+  } else {
+    // if no error
+    User.findOne({ _id }).exec((err, user) => {
+      // search for user in database
+      if (err || !user) {
+        // if no user
         return res.status(404).json({ errorDetails: "User doesn't exist." });
-      } else {                                                  // if user exists
-        if (!user.authenticate(oldPassword)) {                  // compare oldPassword with the password in database
+      } else {
+        // if user exists
+        if (!user.authenticate(oldPassword)) {
+          // compare oldPassword with the password in database
           return res.status(401).json({
             errorDetails: "Incorrect old password",
           });
-        } else {                                                // check if password and confirm password is same
-          if (newchangePassword == confirmchangePassword) {     // if same, update the password
+        } else {
+          // check if password and confirm password is same
+          if (newchangePassword == confirmchangePassword) {
+            // if same, update the password
             const updatedFields = {
               password: newchangePassword,
             };
             user = _.extend(user, updatedFields);
-            user.save((err, result) => {                        // trying to save updated fields in database
-              if (err) {                                        // if error
+            user.save((err, result) => {
+              // trying to save updated fields in database
+              if (err) {
+                // if error
                 res
                   .status(400)
                   .json({ errorDetails: "Error changing password" });
-              } else {                                          // if no error
+              } else {
+                // if no error
                 res
                   .status(200)
                   .json({ responseData: "Password changed successfully" });
               }
             });
-          } else {                                              // if password and confirm password don't match
-            res
-              .status(400)
-              .json({
-                errorDetails:
-                  "Confrim Password doesn't match with New Password.",
-              });
+          } else {
+            // if password and confirm password don't match
+            res.status(400).json({
+              errorDetails: "Confrim Password doesn't match with New Password.",
+            });
           }
         }
       }
