a21345ceeb4a6af6f0ecaae0daacb15629063500
Ashutosh Krishna <ashutoshbritish@gmail.com>
2020-09-13 14:08:02 +0530

    Add auth

--- src/routes/paytm.route.js
+++ src/routes/paytm.route.js
@@ -1,13 +1,13 @@
 // Import libraries here
 const express = require("express");
 const router = express.Router();
-// const auth = require('../helpers/auth')
+const auth = require('../helpers/auth')
 
 // Load Controllers
 const { paytmController, callbackController } = require("../controllers/paytm.controller");
 
 // Routes here
-router.get("/payment", paytmController);
-router.post("/callback", callbackController)
+router.get("/payment", auth, paytmController);
+router.post("/callback",auth, callbackController)
 
 module.exports = router;
