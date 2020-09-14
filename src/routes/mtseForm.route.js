f5f3216985e010c678e35203e9307650b23479e9
hritikhrk <hritik.kmr.111@gmail.com>
2020-09-14 16:22:28 +0530

    fix error handlings and add formSubmitted set to true when form is submitted in userFormSchema

--- src/routes/mtseForm.route.js
+++ src/routes/mtseForm.route.js
@@ -1,49 +1,62 @@
 // import packages
- const express = require('express');
- const bodyParser = require('body-parser');
- const { body, validationResult } = require('express-validator');
- const mongoose = require('mongoose');
- const mtseFormRouter = express.Router();
+const express = require('express');
+const bodyParser = require('body-parser');
+const { body, validationResult } = require('express-validator');
+const mongoose = require('mongoose');
+const mtseFormRouter = express.Router();
 const validUser = require('./validForm.route');
-const authRequired = require('../helpers/auth')
+const authRequired = require('../helpers/auth');
+const authAdmin = require("../helpers/authAdmin");
+const _ = require("lodash");
+const { errorHandler } = require("../helpers/dbErrorHandling");
 
- //import userForm models
- const models = require('../model/userForm.model');
+//import userForm models
+const models = require('../model/userForm.model');
 
- // use application/json body-parser
+// use application/json body-parser
 mtseFormRouter.use(bodyParser.json());
- 
+
 // authentication required
 mtseFormRouter.use(authRequired)
 
-// GET /mtse request
-mtseFormRouter.route('/')
-.get((req,res,next)=>{
-    models.mtseUsers.find({})
-    .then((users)=>{
-        res.statusCode = 200;
-        res.setHeader('Content-Type','application/json');
-        res.json(users);
-    },(err) => next(err))
-    .catch((err) => next(err));
-});
+// GET /mtse/allUsers request
+mtseFormRouter.route('/allUsers/', authAdmin)
+    .get((req, res, next) => {
+        models.mtseUsers.find({})
+            .then((users) => {
+                if (users.length == 0) {
+                    console.log('No MTSE Users found.');
+                    return res.status(404).json({ errorDetails: "No MTSE Users found." });
+                }
+                else {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "application/json");
+                    res.json({ responseData: users });
+                }
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    });
 
-// POST /mtse request
-mtseFormRouter.route('/')
-.post(validUser.validmtse,(req, res, next) => {
+// POST /mtse/register request
+mtseFormRouter.route('/register/',)
+    .post(validUser.validmtse, (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+            console.log(errors);
+            return res.status(422).json({ errors: errors.array() });
         }
-        models.mtseUsers.create(req.body)
-        .then((users) => {
-            console.log('Inserted:\n ',users );
-            res.statusCode=200;
-            res.setHeader('Content-Type','application/json');
-            res.json(users);
-        },(err) => next(err))
-        .catch((err) => next(err));
-});
+        else {
+            req.body.formSubmitted = true;
+            models.mtseUsers.create(req.body)
+                .then((users) => {
+                    console.log('Inserted:\n ', users);
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(users);
+                }, (err) => next(err))
+                .catch((err) => next(err));
+        }
+    });
 
- //export mtseFormRouter
- module.exports = mtseFormRouter;
+//export mtseFormRouter
+module.exports = mtseFormRouter;
--- src/routes/rangotsavForm.route.js
+++ src/routes/rangotsavForm.route.js
@@ -5,7 +5,10 @@ const { body, validationResult } = require('express-validator');
 const mongoose = require('mongoose');
 const rangotsavFormRouter = express.Router();
 const validUser = require('./validForm.route');
-const authRequired = require('../helpers/auth')
+const authRequired = require('../helpers/auth');
+const authAdmin = require("../helpers/authAdmin");
+const _ = require("lodash");
+const { errorHandler } = require("../helpers/dbErrorHandling");
 
 //import userForm models
 const models = require('../model/userForm.model');
@@ -16,34 +19,44 @@ rangotsavFormRouter.use(bodyParser.json());
 // authentication required
 rangotsavFormRouter.use(authRequired)
 
-// GET /rangotsav request
-rangotsavFormRouter.route('/')
-.get((req,res,next)=>{
-   models.rangotsavUsers.find({})
-   .then((users)=>{
-       res.statusCode = 200;
-       res.setHeader('Content-Type','application/json');
-       res.json(users);
-   },(err) => next(err))
-   .catch((err) => next(err));
-});
+// GET /rangotsav/allUsers request
+rangotsavFormRouter.route('/allUsers/', authAdmin)
+    .get((req, res, next) => {
+        models.rangotsavUsers.find({})
+            .then((users) => {
+                if (users.length == 0) {
+                    console.log('No Rangotsav Users found.');
+                    return res.status(404).json({ errorDetails: "No Rangotsav Users found." });
+                }
+                else {
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "application/json");
+                    res.json({ responseData: users });
+                }
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    });
 
-// POST /rangotsav request
-rangotsavFormRouter.route('/')
-.post(validUser.validRangotsav,(req, res, next) => {
-       const errors = validationResult(req);
-       if (!errors.isEmpty()) {
-           return res.status(400).json({ errors: errors.array() });
-       }
-       models.rangotsavUsers.create(req.body)
-       .then((users) => {
-           console.log('Inserted:\n ',users );
-           res.statusCode=200;
-           res.setHeader('Content-Type','application/json');
-           res.json(users);
-       },(err) => next(err))
-       .catch((err) => next(err));
-});
+// POST /rangotsav/register request
+rangotsavFormRouter.route('/register/')
+    .post(validUser.validRangotsav, (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            console.log(errors);
+            return res.status(422).json({ errors: errors.array() });
+        }
+        else {
+            req.body.formSubmitted = true;
+            models.rangotsavUsers.create(req.body)
+                .then((users) => {
+                    console.log('Inserted:\n ', users);
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(users);
+                }, (err) => next(err))
+                .catch((err) => next(err));
+        }
+    });
 
 //export rangotsavFormRouter
 module.exports = rangotsavFormRouter;
