ec65ee3eb6bba83152a5e2fdbb3ca8b37990b0ff
Ashutosh Krishna <ashutoshbritish@gmail.com>
2020-09-16 18:52:44 +0530

    Fix paytm controller

--- src/Paytm/controllers/paytm.controller.js
+++ src/Paytm/controllers/paytm.controller.js
@@ -6,9 +6,9 @@ const Transaction = require("../model/transaction.model");
 const jwt = require("jsonwebtoken");
 
 exports.paytmController = (req, res) => {
-  const token = req.headers.authorization.split(" ");
-  console.log("Headers : ",req.headers)
-  console.log("Token : ",token)// extracting token from header
+  const token = req.headers.authorization.split(" "); // extracting token from header
+  console.log("Headers : ", req.headers);
+  console.log("Token : ", token);
   const { _id } = jwt.decode(token[1]); // decoding _id from token
   User.findOne({ _id }).exec((err, user) => {
     // searching for _id in database
@@ -18,94 +18,97 @@ exports.paytmController = (req, res) => {
         errorDetails: "User doesn't exist.",
       });
     } else {
-      var dict = {
-        MTSE: "20",
-        PUZZLE: "30",
-        FHS: "20",
-        CHESS: "30",
-        RANG: "50",
-      };
+      var userID = user._id;
+    }
+    var dict = {
+      MTSE: "20",
+      PUZZLE: "30",
+      FHS: "20",
+      CHESS: "30",
+      RANG: "50",
+    };
 
-      var formID = req.query.formId;
-      var price = dict[formID];
+    var formID = req.query.formId;
+    var price = dict[formID];
 
-      var paytmParams = {};
+    var paytmParams = {};
 
-      var orderID = String(formID) + new Date().getFullYear() + new Date().getTime();
+    var orderID =
+      String(formID) + new Date().getFullYear() + new Date().getTime();
 
-      paytmParams.body = {
-        requestType: "Payment",
-        mid: process.env.TEST_MERCHANT_ID,
-        websiteName: process.env.WEBSITE,
-        orderId: orderID,
-        callbackUrl: process.env.CALLBACK_URL,
-        txnAmount: {
-          value: String(price),
-          currency: "INR",
-        },
-        userInfo: {
-          custId: user._id,
-        },
-      };
+    paytmParams.body = {
+      requestType: "Payment",
+      mid: process.env.TEST_MERCHANT_ID,
+      websiteName: process.env.WEBSITE,
+      orderId: orderID,
+      callbackUrl: process.env.CALLBACK_URL,
+      txnAmount: {
+        value: String(price),
+        currency: "INR",
+      },
+      userInfo: {
+        custId: userID,
+      },
+    };
 
-      /*
-       * Generate checksum by parameters we have in body
-       */
-      checksum
-        .generateSignature(
-          JSON.stringify(paytmParams.body),
-          process.env.TEST_MERCHANT_KEY
-        )
-        .then(function (checksum) {
-          paytmParams.head = {
-            signature: checksum,
-          };
+    /*
+     * Generate checksum by parameters we have in body
+     */
+    checksum
+      .generateSignature(
+        JSON.stringify(paytmParams.body),
+        process.env.TEST_MERCHANT_KEY
+      )
+      .then(function (checksum) {
+        paytmParams.head = {
+          signature: checksum,
+        };
 
-          var post_data = JSON.stringify(paytmParams);
+        var post_data = JSON.stringify(paytmParams);
 
-          var options = {
-            /* for Staging */
-            hostname: "securegw-stage.paytm.in",
+        var options = {
+          /* for Staging */
+          hostname: "securegw-stage.paytm.in",
 
-            /* for Production */
-            // hostname: 'securegw.paytm.in',
+          /* for Production */
+          // hostname: 'securegw.paytm.in',
 
-            port: 443,
-            path: `/theia/api/v1/initiateTransaction?mid=${process.env.TEST_MERCHANT_ID}&orderId=${orderID}`,
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              "Content-Length": post_data.length,
-            },
-          };
-          var response = "";
-          var post_req = https.request(options, function (post_res) {
-            post_res.on("data", function (chunk) {
-              response += chunk;
+          port: 443,
+          path: `/theia/api/v1/initiateTransaction?mid=${process.env.TEST_MERCHANT_ID}&orderId=${orderID}`,
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "Content-Length": post_data.length,
+          },
+        };
+        var response = "";
+        var post_req = https.request(options, function (post_res) {
+          post_res.on("data", function (chunk) {
+            response += chunk;
+          });
+
+          post_res.on("end", function () {
+            var result = JSON.parse(response);
+            console.log("Response: ", result);
+
+            // Saving txn token in database
+            var transaction = new Transaction({
+              userId: userID,
+              txnToken: result.body.txnToken,
+              orderId: orderID,
+              formId: formID,
+            });
+            transaction.save((err) => {
+              if (err) {
+                console.log("Save error", errorHandler(err));
+                return res.status(500).json({
+                  errorDetails: errorHandler(err),
+                });
+              }
             });
 
-            post_res.on("end", function () {
-              var result = JSON.parse(response);
-              console.log("Response: ", result);
-
-              // Saving txn token in database
-              var transaction = new Transaction({
-                userId: user._id,
-                txnToken: result.body.txnToken,
-                orderId: orderID,
-                formId: formID,
-              });
-              transaction.save((err) => {
-                if (err) {
-                  console.log("Save error", errorHandler(err));
-                  return res.status(500).json({
-                    errorDetails: errorHandler(err),
-                  });
-                }
-              });
-
-              res.writeHead(200, { "Content-Type": "text/html" });
-              res.write(`<html>
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.write(`<html>
                                     <head>
                                         <title>Show Payment Page</title>
                                     </head>
@@ -125,13 +128,12 @@ exports.paytmController = (req, res) => {
                                        </form>
                                     </body>
                                   </html>`);
-              res.end();
-            });
+            res.end();
           });
-
-          post_req.write(post_data);
-          post_req.end();
         });
-    }
+
+        post_req.write(post_data);
+        post_req.end();
+      });
   });
 };
--- src/Paytm/routes/paytm.route.js
+++ src/Paytm/routes/paytm.route.js
@@ -8,7 +8,8 @@ const { paytmController } = require("../controllers/paytm.controller");
 const { callbackController } = require("../controllers/callback.controller");
 
 // Routes here
-router.get("/payment", auth,paytmController);
+router.get("/payment", paytmController);
+router.post("/payment",paytmController)
 router.post("/callback", callbackController);
 // router.get("/transaction", auth, transactionController);
 
