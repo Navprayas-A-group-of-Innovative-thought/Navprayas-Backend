4cc7b5b3c65250dd4e270992de6ffd1ab3764b35
Ashutosh Krishna <ashutoshbritish@gmail.com>
2020-09-16 17:06:37 +0530

    Fix callback controller

--- src/Paytm/controllers/paytm.controller.js
+++ src/Paytm/controllers/paytm.controller.js
@@ -7,8 +7,8 @@ const jwt = require("jsonwebtoken");
 
 exports.paytmController = (req, res) => {
   const token = req.headers.authorization.split(" ");
-  console.log(req.headers)
-  console.log(token)// extracting token from header
+  console.log("Headers : ",req.headers)
+  console.log("Token : ",token)// extracting token from header
   const { _id } = jwt.decode(token[1]); // decoding _id from token
   User.findOne({ _id }).exec((err, user) => {
     // searching for _id in database
